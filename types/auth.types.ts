f7b92ff0e3b2f847a7963561125f5c036c1ceb1d
8harath <8harath.k@gmail.com>
2026-04-24 16:55:00 +0530

    feat: add AuthEvent type

--- types/auth.types.ts
+++ types/auth.types.ts
@@ -65,3 +65,11 @@ export interface SectorFilter {
   excludeSectors: string[]
   minDealCount: number
 }
+
+
+export type AuthEvent =
+  | "SIGNED_IN"
+  | "SIGNED_OUT"
+  | "TOKEN_REFRESHED"
+  | "USER_UPDATED"
+  | "PASSWORD_RECOVERY"
